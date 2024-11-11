9f2c6cb4b09f8bf71e7888848e44fdf7663a90d0
Koen <koen.vandenbrink@student.hu.nl>
2024-11-11 14:43:24 +0100

    fubar

--- nieuw.py
+++
@@ -1,2 +0,0 @@
-def main():
-    print("Dit is de tweede module")
--- tutorial.py
+++ tutorial.py
@@ -110,7 +110,24 @@ def keep(bname):
         if bname in repo.refs:
             repo.git.branch('-D', bname)
         repo.create_head(bname)
+def check_antwoord(antwoord):
+    return antwoord == 42
 
+def hello_world(lang):
+    if lang == "NL":
+        print("Hallo wereld!")
+    elif lang == "FR":
+        print("Bonjour le monde!")
+    else:
+        print("Hello world!")
+
+hello_world("EN")
+
+antwoord = input("Wat is het antwoord op de ultieme vraag van het leven, het universum, en alles?")
+if check_antwoord(antwoord):
+    print("Correct!")
+else:
+    print("Mogelijk heb je een andere vraag in je hoofd?")
 nieuw = """
 def main():
     print("Dit is de tweede module")
